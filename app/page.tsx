49538b8f65cd13592c8e044420a0c91d4af33ade
user <samuel_morgan_@outlook.com>
2024-07-10 09:14:45 +0100

    logo title

--- app/page.tsx
+++ app/page.tsx
@@ -23,37 +23,9 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[#48494b] overflow-hidden cursor-none">
+    <main className="flex min-h-screen flex-col items-center pt-24 bg-[#48494b] overflow-hidden cursor-none">
       <PlusTrackListing />
-      <div className="relative z-[10] flex place-items-center flex-col">
-        <Image
-          style={
-            {
-              // filter: `hue-rotate(${(position.y - 50) * 3.6}deg)`,
-            }
-          }
-          className="absolute"
-          src="/sign.svg"
-          alt="SIGN Logo"
-          width={600}
-          height={600}
-          priority
-        />
-        <Image
-          style={{
-            // filter: `hue-rotate(${(position.y - 50) * 3.6}deg)`,
-            opacity: `${position.x}%`,
-          }}
-          className="relative"
-          src="/plus.svg"
-          alt="PLUS Logo"
-          width={600}
-          height={600}
-          priority
-        />
-      </div>
       <SignTrackListing />
-
       <div className="absolute bottom-0 left-0 m-2">
         <p className="font-mono text-white">
           MOUSE X: {position.x >= 10 ? null : "0"}
@@ -65,7 +37,44 @@ export default function Home() {
         </p>
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-2xl lg:grid-cols-5">
+      <div className="relative z-[10] flex place-items-center flex-col pt-[10rem]">
+        <Image
+          style={
+            {
+              // filter: `hue-rotate(${(position.y - 50) * 3.6}deg)`,
+            }
+          }
+          className="absolute"
+          src="/sign.svg"
+          alt="SIGN Logo"
+          width={300}
+          height={300}
+          priority
+        />
+        <Image
+          style={{
+            // filter: `hue-rotate(${(position.y - 50) * 3.6}deg)`,
+            opacity: `${position.x}%`,
+          }}
+          className="relative"
+          src="/plus.svg"
+          alt="PLUS Logo"
+          width={300}
+          height={300}
+          priority
+        />
+      </div>
+
+      <Image
+        className="relative invert pt-[3rem]"
+        src="/logo.svg"
+        alt="AUTECHRE Logo"
+        width={300}
+        height={300}
+        priority
+      />
+
+      <div className="pt-[2rem] mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-2xl lg:grid-cols-5">
         <h2 className="mb-3 text-2xl font-semibold text-white">SIGN</h2>
         <h2 className="mb-3 text-2xl font-semibold text-white">/</h2>
         <h2 className="mb-3 text-2xl font-semibold text-white">AUTECHRE</h2>
