eaa1ac5b80855fb95d186a45f81592beaf0235be
user <samuel_morgan_@outlook.com>
2024-07-10 13:04:48 +0100

    breaks

--- app/components/SignTrackListing.tsx
+++ app/components/SignTrackListing.tsx
@@ -48,7 +48,9 @@ const SignTrackListing: React.FC<SignTrackListingProps> = ({ opacity }) => {
               <span className="text-stone-500 absolute mx-1 text-[10px] tracking-[1px] top-[-4px] text-nowrap	">
                 ( {el[1]} )
               </span>
-            ) : null}
+            ) : (
+              <br />
+            )}
           </li>
         ) : (
           <br key={i} />
